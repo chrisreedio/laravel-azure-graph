dab16d9b2f0bfd707dedcaa14c6cafd0ba09891e
Chris Reed <chris@reedtech.us>
2023-10-31 03:26:13 -0500

    Added MemberOf Request

---
+++ src/Requests/AzureGraphRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ChrisReedIO\AzureGraph\Requests;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\PaginationPlugin\Contracts\Paginatable;
+
+abstract class AzureGraphRequest extends Request implements Paginatable
+{
+	protected Method $method = Method::GET;
+
+}

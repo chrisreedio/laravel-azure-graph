1f4374836258801b282aab95240ac6d904c35ffb
chrisreedio <chrisreedio@users.noreply.github.com>
2023-10-31 09:06:46 +0000

    Fix styling

--- src/Requests/Users/Group/MemberOfRequest.php
+++ src/Requests/Users/Group/MemberOfRequest.php
@@ -6,19 +6,19 @@ use ChrisReedIO\AzureGraph\Requests\AzureGraphRequest;
 
 class MemberOfRequest extends AzureGraphRequest
 {
-	/**
-	 * @param null|string $userId If null, the request will be made for the current user
-	 */
-	public function __construct(protected ?string $userId = null)
-	{
-	}
+    /**
+     * @param  null|string  $userId If null, the request will be made for the current user
+     */
+    public function __construct(protected ?string $userId = null)
+    {
+    }
 
-	public function resolveEndpoint(): string
-	{
-		if ($this->userId) {
-			return "/users/{$this->userId}/memberOf";
-		}
+    public function resolveEndpoint(): string
+    {
+        if ($this->userId) {
+            return "/users/{$this->userId}/memberOf";
+        }
 
-		return '/me/memberOf';
-	}
+        return '/me/memberOf';
+    }
 }

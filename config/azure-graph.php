d1ca7b54668c63dc6ab16edc6ebb1f7be511e6cd
Chris Reed <chris@reedtech.us>
2023-12-07 04:55:19 -0600

    feat(config): increase pagination limit, add caching
    - Pagination limit in 'azure-graph.php' increased from 100 to 500 to accommodate larger datasets
    - Implemented caching with a default TTL of 24 hours to improve performance and reduce API calls
    - Added cache keys configuration for user deltas, providing a foundation for future caching of groups, roles, and permissions
    
    This change is expected to enhance the user experience by speeding up data retrieval and reducing the load on the external API.

--- config/azure-graph.php
+++ config/azure-graph.php
@@ -3,6 +3,19 @@
 // config for ChrisReedIO/AzureGraph
 return [
     'pagination' => [
-        'limit' => 100,
+        'limit' => 500,
     ],
+    'cache' => [
+        'enabled' => true,
+        'ttl' => 60 * 60 * 24,
+
+        'keys' => [
+            'deltas' => [
+                'users' => 'azure-graph.deltas.users',
+                // 'groups' => 'azure-graph.deltas.groups',
+                // 'roles' => 'azure-graph.deltas.roles',
+                // 'permissions' => 'azure-graph.deltas.permissions',
+            ],
+        ],
+    ]
 ];
